d2d47aaa2a1bed54bf12568b77021d88cfd8b0cc
LadaKa <ladakudlackova@seznam.cz>
2019-06-04 23:15:33 +0200

    vytvoreni podadresaru

--- zapoctak1/init.sh
+++ zapoctak1/init.sh
@@ -1,10 +1,17 @@
 #!/bin/sh
 # rootdir pocetukolu repozitare
-mkdir "./$1/kontrolovac";
-cd "./$1/kontrolovac";
+wd=$(pwd);
+cd "$1";
+mkdir "kontrolovac" 2>/dev/null || 
+        {
+        echo "Nelze inicializovat: V adresari $1 existuje adresar \"kontrolovac\"";
+        exit;
+        }
+cd kontrolovac;
 mkdir "./ukoly" "./studenti";
-mkdir "./ukoly/ukol_{1..$2}";
-mkdir "./ukoly/tmp";
+mkdir "./ukoly/kontroly" "./ukoly/tmp"
+mkdir $(printf "./ukoly/ukol_%02i " $(seq -s ' ' $2));
+mkdir $(printf "./ukoly/kontroly/ukol_%02i " $(seq -s ' ' $2));
 touch "./ukoly/repozitare.txt";
-cd ../..;
-cat "$3" > "./$1/kontrolovac/ukoly/repozitare.txt";
+cd $wd;
+cat "$3" > "$1/kontrolovac/ukoly/repozitare.txt";
