cc6e4d05b712324d4efa437b7f387a08b17be477
LadaKa <ladakudlackova@seznam.cz>
2019-06-13 16:13:11 +0200

    add_ukol.sh

---
+++ zapoctak1/copy_to_ukoly/add_ukol.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+#
+# kontrolovac/ukoly/add_ukol.sh 
+#       add_ukol.sh             Vytvori novy adresar.
+#       add_ukol.sh [N]         Vytvori N novych adresaru.
+
+min=$(ls |grep -c "ukol_");
+min=$(( $min + 1 ));
+if [ $# = 0 ];then
+        max=$min;
+elif ! [[ $1 =~ ^[0-9]+$ ]]; then
+   echo "Chyba: zadany parametr neni cislo." >&2; exit 1;
+else
+        max=$(( $min + $1 ));
+fi;
+mkdir $(printf "ukol_%02i " $(seq -s ' ' $min $max));
+mkdir $(printf "kontroly/ukol_%02i " $(seq -s ' ' $min $max));
